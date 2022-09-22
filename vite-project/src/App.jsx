8397198c1b4dc398428034f15e9bfb0d4dcdbeb5
SokeBiz <oladsodeeq148@gmail.com>
2022-09-22 10:18:19 +0100

    Stage 1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './pages/Navbar'
-import Body from './pages/body'
+import Body from './pages/Body'
 
 function App() {
   const [count, setCount] = useState(0)
--- vite-project/src/pages/Body.jsx
+++ vite-project/src/pages/Body.jsx
@@ -9,14 +9,30 @@ function Body() {
 return(
 <>
 
-<main className="main">
-    aaaa
-    <div>
-        <h1>
-            Your favourite Blog with all the news to get you covered up-to-date
-        </h1>
+<main className="main d-flex bg-secondary">
+    <div className="col-6">
+        
+    </div>
+    <div className="col-6 p-5">
+        <h2>
+            Your favourite Blog with all the latest news to get you covered up-to-date
+        </h2>
+        <h6>
+            We are the currently leading number 1 blog in the whole Nigeria and we bring you news as they are happening
+        </h6>
     </div>
 </main>
+
+<section>
+
+    <h1>
+        Latest news
+    </h1>
+    <div>
+        
+    </div>
+
+</section>
 
 </>        
 )
